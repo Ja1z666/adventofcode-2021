b6b39eddffef51acee9a8a0c06c75c7751ebeba7
Ja1zHF <thatsameuser@icloud.com>
2022-03-23 17:31:41 +0200

    day 1"

---
+++ day1/index.ts
@@ -0,0 +1,9 @@
+import * as fs from 'fs';
+let array = fs.readFileSync('./day1/numbers.txt').toString().split("\n");
+let count = 0;
+
+for(let i = 1; i < array.length; i++){
+    if((parseInt(array[i]) + parseInt(array[i+1]) + parseInt(array[i+2])) > (parseInt(array[i-1]) + parseInt(array[i]) +  parseInt(array[i+1]))) count++;
+}
+
+console.log(count);
